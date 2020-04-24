a2a7701f90c450dca020b9e741c8521bc1c08bda
JuliaPlahotnikova <63021868+JuliaPlahotnikova@users.noreply.github.com>
2020-04-25 00:37:47 +0300

    Add files via upload

---
+++ plahotnikova.julia/lab4/z2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace UsingLib
+{
+    class Program
+    {
+        [DllImport("C:\\Users\\Admin\\Desktop\\lib\\library\\Debug\\library.dll")]
+        public static extern double SquareRect(double Side1, double Side2);
+
+        [DllImport("C:\\Users\\Admin\\Desktop\\lib\\library\\Debug\\library.dll")]
+        public static extern double SquareTr(double Side, double Height);
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter side 1 for rectangle: ");
+            double side1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter side 2 for rectangle: ");
+            double side2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter side for triangle: ");
+            double side = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter height for triangle: ");
+            double height = Convert.ToInt32(Console.ReadLine());
+
+            double Square = SquareRect(side1, side2);
+            double SquareTriangle = SquareTr(side, height);
+
+            Console.WriteLine($" Square of Triangle = {SquareTriangle}");
+            Console.WriteLine($" Square of Rectangle = {Square}");
+
+            Console.ReadKey();
+        }
+    }
+}

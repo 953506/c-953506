1fc8f7bb5bb8a7e165951fa0db69b977bfb17b76
JuliaPlahotnikova <63021868+JuliaPlahotnikova@users.noreply.github.com>
2020-04-24 23:08:10 +0300

    Update Program.cs

--- plahotnikova.julia/lab4/z1/Program.cs
+++ plahotnikova.julia/lab4/z1/Program.cs
@@ -8,17 +8,7 @@ namespace lab4
     {
         [DllImport("winmm.dll")]
         private static extern long mciSendString(string Command, StringBuilder ReturnString, int ReturnLength, IntPtr HCallback);
-        //mcisendstring - функция mci
-        //string cmd - дальний указатель на текстовую управляющую строку
-        //stringbuilder returnstring - указатель на буфер, в который будет записан результат выполнения команды (в текстовом виде). 
-        //Этот параметр можно указать как NULL, если приложение не интересуется результатом выполнения команды
-        //int returnlength - размер буфера для записи результата выполнения команды
-        //intptr hcallback - идентификатор окна, которое получит извещение (сообщение MM_MCINOTIFY) 
-        //после того как устройство завершит операцию.
-        //Этот параметр можно указать как NULL, если извещение не используется
-        //alias - параметр, альтернативное имя для работы с устройством
-        //type waveaudio - тип устройства, через параметр device передается имя файла
-        //это позволит использовать имена файлов с нестандартными расширениями
+        
         static void Main(string[] args)
         {
             Console.WriteLine("Enter path to .wav file:");

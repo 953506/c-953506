623d80c8d0171977e63bf1481ca590ebabf16b66
vladzimir06 <62436317+vladzimir06@users.noreply.github.com>
2020-04-25 00:11:24 +0300

    24.04

---
+++ Koshuba.vladzimir/lab4/4.2(Fin)/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+
+namespace _4._2_Fin_
+{
+    class AlgebraClass
+    {
+       [DllImport("D:\\labolotornaya\\ISP(C#)\\lab4\\Dll2\\Debug\\Dll2")]
+        public static extern double Addition(double _x, double _y);
+        public static extern double deduction(double _x, double _y);
+        public static extern double multiplication(double _x, double _y);
+        public static extern double Divide(double _x, double _y);
+
+        static void Main(string[] args)
+        {
+            double x = 8, y = 4;
+            double result = 0;
+            int choose = int.Parse(Console.ReadLine());
+            switch (choose)
+            {
+                case 1:
+                    {
+                        result = Addition(x, y);
+                        break;
+                    }
+
+                case 2:
+                    {
+                        result = deduction(x, y);
+                        break;
+                    }
+
+                case 3:
+                    {
+                        result = multiplication(x, y);
+                        break;
+                    }
+
+                case 4:
+                    {
+                        result = Divide(x, y);
+                        break;
+                    }
+
+                default:
+                    {
+                        result = 0;
+                        break;
+                    }
+            }
+            Console.WriteLine(result);
+        }
+    }
+}

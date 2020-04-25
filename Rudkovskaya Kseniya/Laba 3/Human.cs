194804effe89943eeb494039aa436ab053456562
kseniya-rs <63047743+kseniya-rs@users.noreply.github.com>
2020-04-25 13:21:54 +0300

    Add files via upload

---
+++ Rudkovskaya Kseniya/Laba 3/Human.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Laba3
+{
+    class Human
+    {
+        private string name = "";
+        private string surname = "";
+        private int age;
+        private string status = "student";
+        private int math, rus, phys;
+        public double score = 0;
+
+        public string Name
+        {
+            get
+            { return name; }
+            set
+            { name = value; }
+        }
+
+        public string Surname
+        {
+            get
+            { return surname; }
+            set
+            { surname = value; }
+        }
+
+        public int Age
+        {
+            get => age;
+            set
+            {
+                if (value <= 16 || value >= 110)
+                {
+                    age = 17;
+                }
+                else
+                    age = value;
+
+            }
+        }
+
+        public string Status
+        {
+            get
+            { return status; }
+            set
+            { status = value; }
+        }
+
+        public Human()
+        {
+            name = "";
+        }
+
+        public Human(string name)
+        {
+            this.name = name;
+        }
+
+        public int Math
+        {
+            set 
+            {
+                Console.WriteLine("Математика: ");
+                Random x = new Random();
+                math = x.Next(1, 11); 
+            }
+            get { return math; }
+        }
+
+        public int Rus
+        {
+            set
+            {
+                Console.WriteLine("Русский: ");
+                Random x = new Random();
+                rus = x.Next(1, 11);
+            }
+            get { return rus; }
+        }
+
+        public int Phys
+        {
+            set
+            {
+                Console.WriteLine("Физика: ");
+                Random x = new Random();
+                phys = x.Next(1, 11);
+            }
+            get { return phys; }
+        }
+
+        public double SetScore()
+        {
+            score = (math + rus + phys) / 3.0;
+            Console.WriteLine($"\nОбщий средний балл: {score}");
+            return score;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Имя:{name}\nФамилия:{surname}\nВозраст:{age} Статус:{status}\n");
+            Console.WriteLine($"Средние баллы:\nМатем:{math}  Рус:{rus}  Физика:{phys}\nОбщий ср.балл:{score}\n");
+        }
+
+        public void AcscessСhange()
+        {
+            char predmet;
+            int change = 0;
+            Console.WriteLine($"Выберите предмет\nМатематика-1  Русский-2  Физика-3");
+            predmet = Console.ReadKey().KeyChar;
+            Console.WriteLine($"На сколько изменить отметку   пример(2 / -4)");
+            int.TryParse(Console.ReadLine(), out change);
+            if (predmet == '1')
+            {
+                if (math + change > 10)
+                {
+                    Console.WriteLine($"Ошибка, повторите ввод снова");
+                }
+                else
+                {
+                    math += change;
+                }
+            }
+            if (predmet == '2')
+            {
+                if (rus + change > 10)
+                {
+                    Console.WriteLine($"Ошибка, повторите ввод снова");
+                }
+                else
+                {
+                    rus += change;
+                }
+            }
+            if (predmet == '3')
+            {
+                if (phys + change > 10)
+                {
+                    Console.WriteLine($"Ошибка, повторите ввод снова");
+                }
+                else
+                {
+                    phys += change;
+                }
+            }
+        }
+
+        public void Expulsion()
+        {
+            status = "otchislen";
+            math = 0;
+            rus = 0;
+            phys = 0;
+            score = 0;
+        }
+
+    }
+
+
+    class Gruppa : Human
+    {
+        Human[] items;
+
+        public Gruppa(int kolv)
+        {
+            items = new Human[kolv + 1];
+        }
+
+        public Human this[int index]
+        {
+            get
+            {
+                return items[index];
+            }
+            set
+            {
+                items[index] = value;
+            }
+        }
+    }
+}
---
+++ Rudkovskaya Kseniya/Laba 3/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+
+namespace Laba3
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int kolv, uchenik;
+            char numer;
+            string name = "Ксения";
+            Console.WriteLine("Сколько студентов в группе? ");
+            int.TryParse(Console.ReadLine(), out kolv);
+            Gruppa chel = new Gruppa(kolv + 1);
+            for (int i = 0; i < kolv; i++)
+            {
+                
+                Console.WriteLine($"\nНомер студента: {i+1}");
+                chel[i] = new Human { };
+                Console.WriteLine("Имя: ");
+                name = Console.ReadLine();
+                chel[i].Name = name;
+                Console.WriteLine("Фамилия: ");
+                string surname = Console.ReadLine();
+                chel[i].Surname = surname;
+                Console.WriteLine("Возраст: ");
+                string age1 = Console.ReadLine();
+                int age = int.Parse(age1);               
+                chel[i].Age = age;
+                chel[i].Math = 1;
+                chel[i].Rus = 1;
+                chel[i].Phys = 1;
+                chel[i].SetScore();
+                Console.Clear();
+            }
+            chel[kolv] = new Human(name);
+            do
+            {
+                Panel();
+                numer = Console.ReadKey().KeyChar;
+
+                switch (numer)
+                {
+                    case '1':
+                        Console.Clear();
+                        for (int i = 0; i < kolv; i++)
+                        {
+                            chel[i].ShowInfo();
+                            Console.WriteLine("\n");
+                        }
+                        break;
+                    case '2':
+                        Console.Clear();
+                        Console.WriteLine($"Выберите номер ученика ");
+                        int.TryParse(Console.ReadLine(), out uchenik);
+                        chel[uchenik - 1].AcscessСhange();
+                        if (chel[uchenik - 1].Math <= 0 || chel[uchenik - 1].Rus <= 0 || chel[uchenik - 1].Phys <= 0)
+                        {
+                            chel[uchenik - 1].Expulsion();
+                        }
+                        chel[uchenik - 1].SetScore();
+                        break;
+                    case '3':
+                        Console.Clear();
+                        for (int i = 0; i < kolv; i++)
+                        {
+                            for (int y = 0; y < kolv - 1; y++)
+                            {
+                                if (chel[y].score < chel[y + 1].score)
+                                {
+                                    chel[kolv + 1] = chel[y];
+                                    chel[y] = chel[y + 1];
+                                    chel[y + 1] = chel[kolv + 1];
+                                }
+                            }
+                        }
+                        break;
+                    case '4':
+                        Console.Clear();
+                        Console.WriteLine($"Выберите номер ученика ");
+                        int.TryParse(Console.ReadLine(), out uchenik);
+                        chel[uchenik - 1].Expulsion();
+                        break;
+                    case '0':
+                        return;
+                }
+            } while (true) ;
+        }
+
+        public static void Panel()
+        {
+            Console.WriteLine("1-вывод всех студентов");
+            Console.WriteLine("2-повысить/понизить балл студента по предметам");
+            Console.WriteLine("3-распределение студентов по убыванию среднего балла");
+            Console.WriteLine("4-отчислить студента");
+            Console.WriteLine("0-Exit");
+
+        }
+    }
+}

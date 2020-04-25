0f4b02f89260360c6dd7a6e5898cb5a358e9ac65
ArtsiomKirychuk <63003764+ArtsiomKirychuk@users.noreply.github.com>
2020-04-25 08:40:53 +0300

    Add files via upload

---
+++ Kirychuk.Artsiom/LR5/Kindergarten.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace laba5
+{   
+    //------------ Структура
+    struct Time
+    {
+        public double atHome;
+        public double inGarten;
+    }
+    //------------ Перечисление
+    enum Mood
+    {
+        Great,
+        Good,
+        Normal,
+        SoSo,
+        Bad,
+    }
+    abstract class Kindergarten
+    {
+        //------------ Поля
+        protected Time _timok = new Time();
+        protected string _name;
+        protected string _surname;
+        protected int _age;
+        protected Mood _spirits= 0;
+
+        //------------ Конструктор
+        public Kindergarten(string name, string surname, int age)
+        {
+            _name = name;
+            _surname = surname;
+            _age = age;
+        }
+
+        //------------Свойства
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public string Surname
+        {
+            get { return _surname; }
+            set { _surname = value; }
+        }
+
+        public int Age
+        {
+            get { return _age; }
+            set
+            {
+                if (value > 0 && value < 100)
+                    _age = value;
+            }
+        }
+
+        public Mood Spirit
+        {
+            get { return _spirits; }
+            set { _spirits = value; }
+        }
+
+        public double GetAtHome()
+        { return _timok.atHome; }
+
+        public void SetAtHome(double atHome)
+        {
+            _timok.atHome = atHome;
+        }
+
+        public double GetInGarten()
+        { return _timok.inGarten; } 
+
+        public void SerInGarten(double inGarten)
+        {
+            _timok.inGarten = inGarten;
+        }
+
+        //------------Методы
+        public abstract void KnowHealth();
+        public abstract void GetInfo();    
+    }
+}
---
+++ Kirychuk.Artsiom/LR5/Pedagogue.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+enum Groups
+{
+    Nursery=2,
+    Younger=3,
+    Middle=4,
+    Senior=5,
+}
+namespace laba5
+{
+    class Pedagogue : Kindergarten
+    {
+        private Groups _group;
+        private int _salary;
+        private string _education;
+
+        //------------ Конструктор
+        public Pedagogue(string name, string surname, int age, Groups group, int salary, string education)
+            :base (name , surname, age)
+        {
+            _group = group;
+            _salary = salary;
+            _education = education;
+        }
+
+        //------------ Свойства
+        public int Salary
+        {
+            get
+            {
+                return _salary;
+            }
+            set
+            {
+                _salary= value / 2;
+            }
+        }
+
+        //------------ Методы
+        public override void GetInfo()
+        {
+            Console.WriteLine($"{_name} {_surname}"); ;
+            Console.WriteLine("Возраст:{0}", _age);
+            Console.WriteLine("Группа детей :{0}", _group);
+            Console.WriteLine("Зарплата(BYN):{0}", _salary);
+            Console.WriteLine("Образование:{0}", _education);
+        }
+ 
+        public override void KnowHealth()
+        {
+            if ((_group == Groups.Senior || _group == Groups.Middle) && _age < 50)
+                Console.WriteLine("Отлично!");
+            if ((_group == Groups.Younger || _group == Groups.Nursery) && _age >= 50)
+                Console.WriteLine("Великолепно. Как может быть иначе!");
+        }
+
+        public void Spirits (int premium)
+        {
+            double percent = (double) premium / (double)_salary * 100;
+            if (percent < 5)
+            {
+                _spirits = Mood.Normal; ;
+                Console.WriteLine("{0} рублей. На телефон положу! ", premium);
+            }
+            if(percent >= 5 && percent <10)
+            {
+                _spirits = Mood.Good;
+                Console.WriteLine("Делаю ремонт!");
+            }
+            if(percent>100)
+            {
+                _spirits = Mood.Great;
+                Console.WriteLine("Заказываю билеты на Бали");
+            }
+        }
+    }
+}

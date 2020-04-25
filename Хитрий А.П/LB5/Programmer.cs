d37b9ef5a04ec860907e57dfcb4fe782207e53db
afk121 <62508193+afk121@users.noreply.github.com>
2020-04-25 09:11:17 +0300

    Update Programmer.cs

--- Хитрий А.П/LB5/Programmer.cs
+++ Хитрий А.П/LB5/Programmer.cs
@@ -17,10 +17,11 @@ sealed class Programmer : Worker
         _programLanguage[0] = "";
     }
 
-    public Programmer(string name, string surname, int age, int high, float weight, string nationality, DateTime birthdate, string parent0, string parent1, string sex,
-        uint salary, uint employmenthistory, uint managerialexperience, uint otherallowances, uint daysofsick, uint daysofvacation, string educacion,
-        string specializacion, string job) : base(name, surname, age, high, weight, nationality, birthdate, parent0, parent1, sex, salary,
-        employmenthistory, managerialexperience, otherallowances, daysofsick, daysofvacation)
+    public Programmer(string name, string surname, int age, int high, float weight, string nationality,
+                      DateTime birthdate, string parent0, string parent1, string sex, uint salary, uint employmenthistory,
+                      uint managerialexperience, uint otherallowances, uint daysofsick, uint daysofvacation, string educacion,
+                      string specializacion, string job) : base(name, surname, age, high, weight, nationality, birthdate,
+                      parent0, parent1, sex, salary, employmenthistory, managerialexperience, otherallowances, daysofsick, daysofvacation)
     {
         Education = educacion;
         Specialization = specializacion;

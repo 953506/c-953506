922e0a8b2374b0bbbb215dd983c0ff556556247d
afk121 <62508193+afk121@users.noreply.github.com>
2020-05-06 16:36:43 +0300

    Update Program.cs

--- Хитрий А.П/LB6/Program.cs
+++ Хитрий А.П/LB6/Program.cs
@@ -1,5 +1,6 @@
-﻿using System;
+using System;
 using static System.Console;
+
 class Program
 {
     static void Menu()
@@ -9,6 +10,7 @@ class Program
             " Q - сравнить класс и шаблонный класс на похожесть данных Только о человеке(не о работе) \n W - обнулить рабочие строки \n" +
             " E - обнулить строки о программировании");
     }
+    
     static void Main()
     {
         ConsoleKeyInfo key;
@@ -17,7 +19,7 @@ class Program
         {
             Clear();
             Menu();
-            switch ((key = ReadKey(true)).Key)
+            switch (ReadKey(true).Key)
             {
                 case ConsoleKey.D0: return;
                 case ConsoleKey.D1: a.Creat(a); WriteLine("Нажмите любую клавишу, чтобы продолжить..."); ReadKey(true); break;

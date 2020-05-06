09ac3fdcdac1b7074a742ef965c3ffef0efb0f3b
953506 <62437619+953506@users.noreply.github.com>
2020-05-06 16:44:51 +0300

    Add files via upload

---
+++ Buyukov.Viktor/Laba6$/Class1.cs
@@ -0,0 +1,155 @@
+﻿class Program
+{
+    static public void WorkerMenu(Workers worker, Director director)
+    {
+        Console.Clear();
+        Console.WriteLine("Input your name:");
+        int number;
+        string name = Console.ReadLine();
+        for (int i = 0; i < 10; i++)
+        {
+            if (worker[i]._name == name)
+                while (true)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("1-see information about you");
+                    Console.WriteLine("2-see list of worker ");
+                    Console.WriteLine("3-see information about boss");
+                    Console.WriteLine("4-Exit program");
+                    number = Convert.ToInt32(Console.ReadLine());
+
+                    switch (number)
+                    {
+                        case 1:
+                            {
+                                worker[i].WriteCommonInformation();
+                                worker[i].WriteCarParkInformation();
+                                worker[i].WriteImportantInformation();
+                            }
+                            break;
+
+                        case 3:
+                            {
+                                director.WriteCommonInformation();
+                                director.WriteCarParkInformation();
+                                director.WriteImportantInformation();
+                            }
+                            break;
+
+                        case 2:
+                            {
+                                worker.Rating();
+                            }
+                            break;
+
+                        case 5: break;
+
+                        default:
+                            Console.WriteLine("Введено неверное действие");
+                            break;
+
+                    }
+                }
+        }
+    }
+
+    static public void DirectorMenu(Workers worker, Director director)
+    {
+        Console.Clear();
+        int number = 0;
+        while (true)
+        {
+            Console.WriteLine("1-Enter Information about yourself");
+            Console.WriteLine("2-Check Information about yourself");
+            Console.WriteLine("3-see information about worker");
+            Console.WriteLine("4-add information about worker");
+            Console.WriteLine("5-check lists of worer");
+            Console.WriteLine("6-exit program");
+            number = Convert.ToInt32(Console.ReadLine());
+
+            switch (number)
+            {
+                case 1: director.EnterImportantInformation(); break;
+                case 2:
+                    {
+                        director.WriteCommonInformation();
+                        director.WriteCarParkInformation();
+                        director.WriteImportantInformation();
+                        Console.WriteLine();
+                    }
+                    break;
+
+                case 3:
+                    for (int s = 0; s < 10; s++)
+                    {
+                        worker[s].WriteCommonInformation();
+                        worker[s].WriteCarParkInformation();
+                        worker[s].WriteImportantInformation();
+
+                        Console.WriteLine();
+                    }
+                    break;
+
+                case 4:
+                    {
+                        Console.WriteLine("Введите имя студента:");
+                        string sname = Console.ReadLine();
+                        for (int s = 0; s < 10; s++)
+                        {
+                            if (worker[s]._name != sname)
+
+                                continue;
+                            else
+                            {
+                                worker[s].EnterImportantInformation();
+                                break;
+                            }
+                        }
+                    }
+                    break;
+
+                case 5: worker.Rating(); break;
+                case 6: return;
+                default: Console.WriteLine("Введено неверное действие"); break;
+            }
+        }
+    }
+
+    static void Main(string[] args)
+    {
+
+        Workers worker = new Workers();
+        worker[0] = new Worker("A", "A", "Park", 18, "Worker", 241);
+        worker[1] = new Worker("B", "B", "Park", 19, "Worker", 242);
+        worker[2] = new Worker("C", "C", "Park", 20, "Worker", 243);
+        worker[3] = new Worker("D", "D", "Park", 21, "Worker", 244);
+        worker[4] = new Worker("F", "F", "Park", 22, "Worker", 245);
+        worker[5] = new Worker("E", "E", "Park", 24, "Worker", 246);
+        worker[6] = new Worker("G", "G", "Park", 26, "Worker", 247);
+        worker[7] = new Worker("K", "K", "Park", 28, "Worker", 246);
+        worker[8] = new Worker("N", "N", "Park", 39, "Worker", 248);
+        worker[9] = new Worker("M", "M", "Park", 45, "Worker", 246);
+
+        Director director = new Director("Michael", "Lukashevich", "BSUIR", 52, "Boss of the gym");
+
+        int number;
+        while (true)
+        {
+            Console.Clear();
+            Console.WriteLine("Who are you?");
+            Console.WriteLine("1-Boss");
+            Console.WriteLine("2-Worker");
+            Console.WriteLine("3-Exit program");
+            number = Convert.ToInt32(Console.ReadLine());
+
+            switch (number)
+            {
+                case 2: WorkerMenu(worker, director); break;
+                case 1: DirectorMenu(worker, director); break;
+                case 3: return;
+                default: Console.WriteLine("Incorect input"); break;
+            }
+        }
+
+    }
+}
---
+++ Buyukov.Viktor/Laba6$/Program.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+namespace Laba6New
+{
+    interface ICar_Park_Person
+    {
+        string _name { get; }
+        string _surname { get; }
+        string _car_park { get; }
+        int _age { get; }
+        public string ImportantInformation { get; }
+
+        void WriteCommonInformation();
+        void EnterImportantInformation();
+        void WriteImportantInformation();
+
+    }
+
+    abstract class Car_Park_Person : ICar_Park_Person
+    {
+        public string _name { get; }
+        public string _surname { get; }
+        public string _car_park { get; }
+        public int _age { get; }
+        public string ImportantInformation { get; }
+
+        public Car_Park_Person(string name, string surname, string park, int age)
+        {
+            _name = name;
+            _surname = surname;
+            _car_park = park;
+            _age = age;
+        }
+
+        public abstract void WriteCommonInformation();
+        public abstract void WriteCarParkInformation();
+        public abstract void EnterImportantInformation();
+        public abstract void WriteImportantInformation();
+    }
+
+    interface IDirector
+    {
+      string _status { get; } 
+      string _information { get; set; }
+
+      void Put_Hours_of_Work(Worker worker);
+    }
+
+    class Director : Car_Park_Person
+    {
+        public string _status;
+        public string _information { get; set; }
+
+        public Director(string name, string surname, string park, int age, string status)
+            : base(name, surname, park, age)
+        {
+            _status = status;
+        }
+
+        public override void WriteCommonInformation()
+        {
+            Console.WriteLine($"Name:{_name}");
+            Console.WriteLine($"Surname:{_surname}");
+            Console.WriteLine($"Age:{_age}");
+        }
+
+        public override void WriteCarParkInformation()
+        {
+            Console.WriteLine($"Car park:{_car_park}");
+        }
+        public override void EnterImportantInformation()
+        { 
+
+            _information = Console.ReadLine();
+        }
+
+        public override void WriteImportantInformation()
+        {
+
+        }
+
+        public void Put_Hours_of_Work(Worker worker)
+        {
+            worker._money = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+
+    interface IWorker
+    {
+        public string _specialty { get; set; }
+        public int _money { get; set; }
+        string _information { get; set; }
+        void Min();
+    }
+
+    class Worker : Car_Park_Person,IWorker,ICloneable 
+    {
+        public string _specialty { get; set; }
+        public string _information { get; set; }
+        public int _money { get; set; } 
+
+        public Worker(string name, string surname, string park, int age, string specialty, int _money)
+           : base(name, surname, park, age)
+        {
+            _specialty = specialty;
+            this._money = _money;
+        }
+
+        public void Min()
+        {
+            Console.WriteLine($"His amount of money:{_money}");
+            if (Can_you_survive())        
+                Console.WriteLine("Good, that enough");
+            else
+                Console.WriteLine("He don't survive");
+            
+        }
+
+        private bool Can_you_survive()
+        {
+            if (_money > 240)
+                return true;
+            else 
+                return false;
+        }
+
+        public object Clone()
+        {
+            return this.MemberwiseClone();
+        }
+
+        public override void WriteCommonInformation()
+        {
+            Console.WriteLine($"Name:{_name}");
+            Console.WriteLine($"Surname:{_surname}");
+            Console.WriteLine($"Age:{_age}");
+        }
+
+        public override void WriteCarParkInformation()
+        {
+            Console.WriteLine($"Car park:{_car_park}");
+        }
+        public override void EnterImportantInformation()
+        {
+            _information = Console.ReadLine();
+        }
+
+        public override void WriteImportantInformation()
+        {
+            Console.WriteLine($"Information about worker : {_information}");
+        }
+    }
+
+    class Workers : IComparer<IWorker>
+    {
+        Worker[] work;
+
+        public Workers()
+        {
+            work = new Worker[10];
+        }
+
+        public Worker this[int index]
+        {
+            get
+            {
+                return work[index];
+            }
+
+            set
+            {
+                work[index] = value;
+            }
+        }
+
+        public int Compare(IWorker w1, IWorker w2)
+        {
+            if (w1._money > w2._money)
+                return 1;
+            else if (w1._money < w2._money)
+                return -1;
+            else
+                return 0;
+        }
+
+        public void Rating()
+
+        {
+
+            for (int i = 1; i < 10; i++)
+                for (int j = 0; j < 9; j++)
+                    if (Compare(work[j + 1], work[j]) == 1)
+                    {
+                        Worker st = (Worker)work[j].Clone();
+                        work[j] = (Worker)work[j + 1].Clone();
+                        work[j + 1] = (Worker)st.Clone();
+                    }
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"{work[i]._name} {work[i]._surname} - ({work[i]._money})");
+            }
+        }
+
+
+
+    }
+}

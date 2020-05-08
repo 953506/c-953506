06cca9ad8de0882b2bef16b314020a70578576ea
KarpenkoIgor <62311623+KarpenkoIgor@users.noreply.github.com>
2020-05-08 17:40:41 +0300

    LR7

---
+++ karpenko.igor/LR7/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace z1
+{
+    class Program
+    {
+        static void Menu() 
+        {
+            Console.WriteLine();
+            Console.WriteLine("Выберите действие:");
+            Console.WriteLine("1)Сложить два числа");
+            Console.WriteLine("2)Вычесть из первого числа второе");
+            Console.WriteLine("3)Умножить числа");
+            Console.WriteLine("4)Разделить первое число на второе");
+            Console.WriteLine("5)Показать большее число");
+            Console.WriteLine("6)Показать меньшее число");
+            Console.WriteLine("7)Показать, равны ли числа");
+            Console.WriteLine("8)Выход");           
+        }
+        static void Main(string[] args)
+        {
+            RationalNumber a, b;
+            int numerator, denominator;
+            Console.WriteLine("Введите числитель первой дроби");
+            numerator = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите знаменатель первой дроби");
+            denominator = Convert.ToInt32(Console.ReadLine());
+            a = new RationalNumber(numerator, denominator);
+            Console.WriteLine("Введите числитель второй дроби");
+            numerator = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите знаменатель второй дроби");
+            denominator = Convert.ToInt32(Console.ReadLine());
+            b = new RationalNumber(numerator, denominator);
+            int ch;
+            bool exit = false;
+            do
+            {
+                Menu();
+                ch = Convert.ToInt32(Console.ReadLine());
+                Console.Clear();
+                switch (ch) 
+                {
+                    case 1:
+                        Console.WriteLine($"{a + b} или {(double)(a + b)}");
+                        break;
+                    case 2:
+                        Console.WriteLine($"{a - b} или {(double)(a - b)}");
+                        break;
+                    case 3:
+                        Console.WriteLine($"{a * b} или {(double)(a * b)}");
+                        break;
+                    case 4:
+                        Console.WriteLine($"{a / b} или {(double)(a / b)}");
+                        break;
+                    case 5:
+                        if(a > b) Console.WriteLine($"{a} или {(double)(a)} больше");
+                        else if(a < b) Console.WriteLine($"{b} или {(double)(b)} больше");
+                        else Console.WriteLine("Числа не равны");
+                        break;
+                    case 6:
+                        if (a < b) Console.WriteLine($"{a} или {(double)(a)} меньше");
+                        else if (a > b) Console.WriteLine($"{b} или {(double)(b)} меньше");
+                        else Console.WriteLine("Числа не равны");
+                        break;
+                    case 7:
+                        if (a == b) Console.WriteLine("Числа равны");
+                        else Console.WriteLine("Числа не равны");
+                        break;
+                    case 8:
+                        exit = true;
+                        break;
+                    default:
+                        Console.WriteLine("Что-то не так.");
+                        break;
+                }
+            } while (!exit);
+        }
+    }
+}
---
+++ karpenko.igor/LR7/RationalNumber.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace z1
+{
+    class RationalNumber : IComparable, IEquatable<RationalNumber>
+    {
+        private readonly int _numerator;
+        private readonly int _denominator;
+        public RationalNumber(int numerator, int denominator)
+        {
+            _numerator = numerator;
+            _denominator = denominator;
+        }
+        public static RationalNumber operator +(RationalNumber a, RationalNumber b) 
+        {
+            RationalNumber res;
+            res = new RationalNumber(a._numerator * b._denominator + b._numerator * a._denominator , a._denominator * b._denominator);
+            return res;
+        }
+        public static RationalNumber operator -(RationalNumber a, RationalNumber b)
+        {
+            RationalNumber res;
+            res = new RationalNumber(a._numerator * b._denominator - b._numerator * a._denominator, a._denominator * b._denominator);
+            return res;
+        }
+        public static RationalNumber operator *(RationalNumber a, RationalNumber b)
+        {
+            RationalNumber res;
+            res = new RationalNumber(a._numerator * b._numerator, a._denominator * b._denominator);
+            return res;
+        }
+        public static RationalNumber operator /(RationalNumber a, RationalNumber b)
+        {
+            RationalNumber res;
+            res = new RationalNumber(a._numerator * b._denominator, a._denominator * b._numerator);
+            return res;
+        }
+
+        public static bool operator <(RationalNumber a, RationalNumber b)
+        {
+            if (a.CompareTo(b) == -1)
+                return true;
+            else return false;
+        }
+        public static bool operator >(RationalNumber a, RationalNumber b)
+        {
+            if (a.CompareTo(b) == 1)
+                return true;
+            else return false;
+        }
+        public static bool operator >=(RationalNumber a, RationalNumber b)
+        {
+            if (a.CompareTo(b) == 1|| a.CompareTo(b) == 0)
+                return true;
+            else return false;
+        }
+        public static bool operator <=(RationalNumber a, RationalNumber b)
+        {
+            if (a.CompareTo(b) == -1 || a.CompareTo(b) == 0)
+                return true;
+            else return false;
+        }
+        public static bool operator ==(RationalNumber a, RationalNumber b)
+        {
+            return a.Equals(b);
+        }
+        public static bool operator !=(RationalNumber a, RationalNumber b)
+        {
+            return a.Equals(b);
+        }
+        public int CompareTo(object o) 
+        {
+            RationalNumber num = o as RationalNumber;
+            if (num is RationalNumber)
+            {
+                if (_numerator * num._denominator < num._numerator * _denominator)
+                    return -1;
+                if (_numerator * num._denominator > num._numerator * _denominator)
+                    return 1;
+                return 0;
+           }
+            else
+                throw new Exception("Невозможно сравнить два объекта");
+        }
+
+        public bool Equals(RationalNumber a) 
+        {
+            if (a is null)
+                return false;
+            return _numerator * a._denominator == a._numerator * _denominator;
+        }
+        public override bool Equals(Object obj)
+        {
+            if (obj == null)
+                return false;
+
+            RationalNumber num = obj as RationalNumber;
+            if (num == null)
+                return false;
+            else
+                return Equals(num);
+        }
+        public static implicit operator double(RationalNumber a)
+        {
+            return (double)a._numerator / a._denominator;
+        }
+
+        public static explicit operator int(RationalNumber a)
+        {
+            return a._numerator / a._denominator;
+        }
+        public override string ToString()
+        {
+            return ($"{_numerator}/{_denominator}");
+        }
+        ~RationalNumber() { }
+    }
+}

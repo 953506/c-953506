945f62355befb225a994fe957a3fdc6acee08e40
953506 <62437619+953506@users.noreply.github.com>
2020-05-09 12:20:40 +0300

    Add files via upload

---
+++ Grishan.Ruslan/5/abitur.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Laba5
+{
+    class Abiturient : Human
+    {
+        protected double _attestat, _balli;
+        protected new bool _army = false;
+        public bool iitp, poit, vmsis;
+        protected string _current_status;
+        public Abiturient():base("Ruslan", "Luksha","Sapsanovich")
+        {
+            Attestat = 8.7;
+            Lng = 75;
+            Math = 73;
+            Physics = 70;
+            _balli = _attestat + ct.math + ct.physics + ct.lng;
+        }
+        public struct Predmets
+        {
+            public int physics, math, lng;
+        };
+        Predmets ct = new Predmets();
+        enum Budjet
+        {
+            max = 400,
+            poit = 360,
+            iitp = 356,
+            vmsis = 341
+        };
+        enum Platnoe                        
+        {
+            poit = 309,
+            iitp = 290,
+            vmsis = 265,
+            min = 0
+        };
+        public double Balli
+        { 
+            get
+            {
+                return _balli;
+            }
+            set 
+            {
+                _balli = value;
+            }
+        
+        }
+        public virtual string Status 
+        {
+            get 
+            {
+                return _current_status; 
+            } 
+            set 
+            { 
+                _current_status = "School boy"; 
+            } 
+        }
+        public int Physics
+        {
+            set 
+            {
+                if (value > 0 && value < 101)
+                { 
+                    ct.physics = value; 
+                }
+                
+            } 
+        }
+        public double Attestat
+        {
+            set
+            {
+                if (value > 0 && value <= 10.0)
+                { 
+                    _attestat = value*10; 
+                }
+
+            }
+        }
+        public int Math
+        {
+            set
+            {
+                if (value > 0 && value < 101)
+                { 
+                    ct.math = value; 
+                }
+
+            }
+        }
+        public int Lng
+        {
+            set
+            {
+                if (value > 0 && value < 101)
+                { 
+                    ct.lng = value; 
+                }
+
+            }
+        }
+        public override int Age 
+        {
+            set 
+            {
+                if (value > 16)
+                {
+                    _age = value; 
+                }
+            } 
+        }          
+        //методы
+        public void WriteBalli()
+        {
+            Console.WriteLine("Write Attestat");
+            Attestat=double.Parse(Console.ReadLine());
+            Console.WriteLine("Write Math");
+            Math=int.Parse(Console.ReadLine());
+            Console.WriteLine("Write Languge");
+            Lng=int.Parse(Console.ReadLine());
+            Console.WriteLine("Write Physics");
+            Physics=int.Parse(Console.ReadLine());
+            Balli = ct.physics + ct.math + ct.lng + _attestat;
+        }
+        public void Proxod()
+        {
+            if (_balli <= 400 && _balli >= (double)Budjet.poit)
+            { 
+                poit = true; 
+            }
+            if (_balli >= (double)Platnoe.poit && _balli <(double)Budjet.vmsis) 
+            { 
+                poit = true; 
+            }
+            if (poit == false)
+            {
+                if (_balli < (double)Budjet.poit && _balli >= (double)Budjet.iitp)
+                {
+                    iitp = true; 
+                }
+                if (_balli >= (double)Platnoe.iitp && _balli < (double)Platnoe.poit)
+                {
+                    iitp = true; 
+                }
+            }
+            if (poit == false && iitp == false)
+            {
+                if (_balli < (double)Budjet.iitp && _balli >= (double)Budjet.vmsis)
+                { 
+                    vmsis = true; 
+                }
+                if (_balli >= (double)Platnoe.vmsis && _balli < (double)Platnoe.iitp)
+                { 
+                    vmsis = true; 
+                }
+            }
+            if (poit == false && iitp == false && vmsis == false)
+            { 
+                Console.WriteLine("Welcome to the army!"); 
+                _army = true; 
+            }
+        }
+       
+
+    }
+}
---
+++ Grishan.Ruslan/5/poit.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Laba5
+{
+    sealed class Poit:Abiturient
+    {
+        private int _course;
+        //конструктор 
+        public Poit() : base()
+        {
+            Console.WriteLine("Write course");
+            Kurs = int.Parse(Console.ReadLine());
+            Status = "Poit student";
+        }
+        public override string Status
+        {
+            get => base.Status;
+            set
+            {
+                _current_status = value;
+            }
+        }
+
+        enum Semesetr
+        {
+            First = 11,
+            Second = 10,
+            Thirst = 8,
+            Fourth = 7
+        }
+        public int Kurs
+        {
+            set
+            {
+                if (value > 0 && value < 5)
+                {
+                    _course = value;
+                }
+                else
+                {
+                    Console.WriteLine("Write normal number from 1 to 4");
+                    Console.ReadKey();
+                }
+            }
+
+        }
+
+        public void SemPredm()
+        {
+            switch (_course)
+            {
+                case 1: Console.WriteLine($"You have {(int)Semesetr.First} subjects"); break;
+                case 2: Console.WriteLine($"You have {(int)Semesetr.Second} subjects"); break;
+                case 3: Console.WriteLine($"You have {(int)Semesetr.Thirst} subjects"); break;
+                case 4: Console.WriteLine($"You have {(int)Semesetr.Fourth} subjects"); break;
+                default: Console.WriteLine("Write normal course"); return;
+            }
+
+        }
+
+        public void Vivod()
+        {
+            Console.WriteLine($"FIO:{_surname} {_name } {_patronymic}");
+            Console.WriteLine($"Age: {_age}");
+            Console.WriteLine($"Current status:{Status}");
+            Console.WriteLine($"Your course:{_course}");
+            Console.WriteLine($"Current army status:{_army}");
+        }
+        public void Vivod(string status)
+        {
+            Status = status;
+            Console.WriteLine($"FIO:{_surname} {_name } {_patronymic}");
+            Console.WriteLine($"age: {_age}");
+            Console.WriteLine($"Current status:{Status}");
+            Console.WriteLine($"Your course:{_course}");
+            Console.WriteLine($"Your ct balli:{Balli}");
+            Console.WriteLine($"Current army status:{_army}");
+        }
+        public new void Menu()
+        {
+            Console.WriteLine("1) Show inforamiton");
+            Console.WriteLine("2) Show info with new status");
+            Console.WriteLine("3) Show your subjects");
+            Console.WriteLine("4) Change course");
+            Console.WriteLine("5) Show human info");
+            Console.WriteLine("0) Exit");
+        }
+    }
+}

8afde1c00950f067b9d40f467efa09dc649a34a2
BoyNextDoorxBY <egkuzoff@gmail.com>
2020-05-13 15:29:24 +0300

    Added lab 8.

---
+++ kuzovkov.yegor/Lab 8/IMovable.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Lab_8_Ind_1 
+{
+    interface IMovable
+    {
+        public const int minSpeed = 0;
+        private static int _maxSpeed = 80;
+		
+        public int MaxSpeed
+        {
+            get { return _maxSpeed;}
+            set { _maxSpeed = value; }
+        }
+		
+        public double GetTime(double distance, double speed) => distance / speed;
+		
+    }
+}

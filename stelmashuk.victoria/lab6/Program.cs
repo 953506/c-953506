f836e60c4a1c2c82cae30829762c6c8b7a921a69
victoriastelmashuk <62291292+victoriastelmashuk@users.noreply.github.com>
2020-05-17 08:06:58 +0300

    Update Program.cs

--- stelmashuk.victoria/lab6/Program.cs
+++ stelmashuk.victoria/lab6/Program.cs
@@ -79,7 +79,7 @@ namespace lab6
         }
     }
 
-    class PersonCompare : IComparison<Person>
+    class PersonComparison : IComparison<Person>
     {
         public void AgeComparison(Person o1, Person o2)//неявная реализация метода
         {
@@ -199,20 +199,20 @@ namespace lab6
                     }
                 case 2:
                     {
-                        PersonCompare personComparer = new PersonCompare();
+                        PersonComparison personComparison = new PersonComparison();
                         Console.WriteLine("Сравнение параметров мужчин: ");
-                        personComparer.AgeComparison(person1, person2);
-                        personComparer.HeightComparison(person1, person2);
-                        personComparer.WeightComparison(person1, person2);
+                        personComparison.AgeComparison(person1, person2);
+                        personComparison.HeightComparison(person1, person2);
+                        personComparison.WeightComparison(person1, person2);
                         break;
                     }
                 case 3:
                     {
-                        PersonCompare personComparer = new PersonCompare();
+                        PersonComparison personComparison = new PersonComparison();
                         Console.WriteLine("Сравнение параметров женщин: ");
-                        personComparer.AgeComparison(person3, person4);
-                        personComparer.HeightComparison(person3, person4);
-                        personComparer.WeightComparison(person3, person4);
+                        personComparison.AgeComparison(person3, person4);
+                        personComparison.HeightComparison(person3, person4);
+                        personComparison.WeightComparison(person3, person4);
                         break;
                     }
                 case 4:

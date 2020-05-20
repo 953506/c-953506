3bd063d7d524cd28d0037d9e253992f34931bcb5
Dubrovskiy-Konstantin <62993410+Dubrovskiy-Konstantin@users.noreply.github.com>
2020-05-20 17:08:57 +0300

    Update Program.cs

--- Dubrovskiy-Konstantin/laba6/Program.cs
+++ Dubrovskiy-Konstantin/laba6/Program.cs
@@ -10,20 +10,16 @@ namespace ConsoleApp8
             var pig2 = new Pig<int>(145);       //свинья весом <int> 145 кг
             var sheep = new Sheep<int>(800);    //овечка с <int> 800 грамм шерсти
             var cow = new Cow<string>("4");     //корова с <string> "4" литрами млолка
-
             IFarmer cow_farmer = new Cow<int>(10); //объект интерфейса можно создать присвоив ему объект класса, в котором он реализован
             IAnimal sheep_animal = sheep;
             IAnimal pig1_animal = pig1;
             IFarmer pig2_farmer = pig2;
-
             cow.Voice(); //мы можем вызвать метод Voice т.к. он был неявно реализован как член интерфейса
             sheep_animal.Voice(); //а можем вызвать и через интерфейс
             cow_farmer.Info();  //вызываем явно реализованный метод интерфейса
             Console.WriteLine(cow_farmer.Use());
-
             cow_farmer = pig2_farmer; //можно присвоить друг другу элементы одинаковых интерфейсов (IFarmer)
             Console.WriteLine(cow_farmer.Use()); //вызовется метод из Pig
-
             var x = new Cow<int>(7);
             var y = new Cow<int>(9);
             if (x.Milk.CompareTo(y.Milk) > 0) //Сравнение при помощи IComperable

dbc08da7759a69fa8db76635fbdcf59ae1270720
ArtsiomKirychuk <63003764+ArtsiomKirychuk@users.noreply.github.com>
2020-05-27 15:40:53 +0300

    Add LR7

---
+++ Kirychuk.Artsiom/LR7/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections;
+using System.ComponentModel.Design;
+
+namespace lr7
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            RationalNumber num1, num2, result;
+            ArrayList numbers = new ArrayList();
+            string input;
+            Console.WriteLine("input format" +
+                "\n a/b" +
+                "\n a b" +
+                "\n a is an integer" +
+                "\n b is a natural namber");
+            Console.WriteLine("first number");
+            input = Console.ReadLine();
+            RationalNumber.Initialization(input,out num1);
+            Console.WriteLine("second number");
+            input = Console.ReadLine();
+            RationalNumber.Initialization(input, out num2);
+            int menu;
+            Menu();
+            while (!Int32.TryParse(Console.ReadLine(), out menu))
+                Console.WriteLine("incorrect input");
+            while(true)
+            {
+                if(menu==1)
+                {
+                    Console.Clear();
+                    result = num1 + num2;
+                    Console.WriteLine(RationalNumber.ToStringIrreducible(result));
+                    Console.WriteLine(RationalNumber.ToString(result));
+                    numbers.Add(result);
+                }
+                if (menu==2)
+                {
+                    Console.Clear();
+                    result = num1 - num2;
+                    Console.WriteLine(RationalNumber.ToStringIrreducible(result));
+                    Console.WriteLine(RationalNumber.ToString(result));
+                    numbers.Add(result);
+                }
+                if (menu==3)
+                {
+                    Console.Clear();
+                    result = num1 * num2;
+                    Console.WriteLine(RationalNumber.ToStringIrreducible(result));
+                    Console.WriteLine(RationalNumber.ToString(result));
+                    numbers.Add(result);
+                }
+                if (menu==4)
+                {
+                    Console.Clear();
+                    result = num1 / num2;
+                    Console.WriteLine(RationalNumber.ToStringIrreducible(result));
+                    Console.WriteLine(RationalNumber.ToString(result));
+                    numbers.Add(result);
+                }
+                if(menu==5)
+                {
+                    Console.Clear();
+                    if (num1 > num2)
+                        Console.WriteLine(RationalNumber.ToString(num1) + ">"+ RationalNumber.ToString(num2));
+                    if (num1 <num2)
+                        Console.WriteLine(RationalNumber.ToString(num1) + "<" + RationalNumber.ToString(num2));
+                    if (num1 == num2)
+                        Console.WriteLine(RationalNumber.ToString(num1) + "=" + RationalNumber.ToString(num2));
+                    if (num1 != num2)
+                        Console.WriteLine(RationalNumber.ToString(num1) + "!=" + RationalNumber.ToString(num2));
+                    if (num1 <= num2)
+                        Console.WriteLine(RationalNumber.ToString(num1) + "<=" + RationalNumber.ToString(num2));
+                    if (num1 >=num2)
+                        Console.WriteLine(RationalNumber.ToString(num1) + ">=" + RationalNumber.ToString(num2));
+                }
+                if(menu==6)
+                {
+                    Console.Clear();
+                    double implis1 = num1, implis2 = num2;
+                    int expl1 = (int)num1, expl2 = (int)num2;
+                    Console.WriteLine($"double(num1)={implis1};   double(num2)={implis2}");
+                    Console.WriteLine($"   int(num1)={expl1};        int(num2)={expl2}");
+                }
+                if(menu==7)
+                {
+                    Console.Clear();
+                    numbers.Sort();
+                    foreach(RationalNumber o in numbers )
+                    {
+                        double a = o;
+                        Console.WriteLine(a);
+                    }
+                }
+                if(menu==8)
+                {
+                    Console.Clear();
+                    Console.WriteLine("program completed");
+                    Environment.Exit(1);
+                }
+                Menu();
+                while (!Int32.TryParse(Console.ReadLine(), out menu))
+                    Console.WriteLine("incorrect input");
+            }               
+        }
+
+        public static void Menu()
+        {
+            Console.WriteLine("1.Sum" +
+                "\n2.Residual" +
+                "\n3.Multiplication" +
+                "\n4.Division" +
+                "\n5.Compare" +
+                "\n6.ToInt ToDouble" +
+                "\n7.Sorting" +
+                "\n8.Exit");              
+        }        
+    }
+}
---
+++ Kirychuk.Artsiom/LR7/RationalNumber.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace lr7
+{
+
+    class RationalNumber: IEquatable<RationalNumber>, IComparable
+    {
+
+        //------------Поля
+        private int _numerator;
+        private int _denominator;
+        
+        //--------Конструктор
+        public RationalNumber (int integer, int naturalNumber )
+        {
+            _numerator = integer;
+            while (naturalNumber == 0)
+                naturalNumber = Convert.ToInt32(Console.ReadLine());
+            _denominator = naturalNumber;
+        }
+
+        //---------Перегрузка операторов
+        public static RationalNumber operator + (RationalNumber num1, RationalNumber num2)
+        {
+            RationalNumber result = new RationalNumber((num1._numerator * num2._denominator + num2._numerator * num1._denominator), num1._denominator * num2._denominator);
+            return result;
+        }
+
+        public static RationalNumber operator - (RationalNumber num1, RationalNumber num2)
+        {
+            RationalNumber result =  new RationalNumber((num1._numerator * num2._denominator - num2._numerator * num1._denominator), num1._denominator * num2._denominator);
+            return result;
+        }
+
+        public static RationalNumber operator *(RationalNumber num1, RationalNumber num2)
+        {
+            RationalNumber result = new RationalNumber(num1._numerator * num2._numerator, num1._denominator * num2._denominator);
+            return result;
+        }
+
+        public static RationalNumber operator /(RationalNumber num1, RationalNumber num2)
+        {             
+                RationalNumber result = new RationalNumber(num1._numerator * num2._denominator, num1._denominator * num2._numerator);
+                return result;          
+        }
+
+        public static bool operator >(RationalNumber num1, RationalNumber num2)
+        {
+            return num1 >(double) num2;
+        }
+
+        public static bool operator < (RationalNumber num1, RationalNumber num2)
+        {
+            return num1 >(double) num2;
+        }
+
+        public static bool operator >=(RationalNumber num1, RationalNumber num2)
+        {
+            return num1 >=(double) num2;
+        }
+
+        public static bool operator <=(RationalNumber num1, RationalNumber num2)
+        {
+            return num1 <=(double) num2;
+        }
+
+        public static bool operator ==(RationalNumber num1, RationalNumber num2)
+        {
+            return num1.Equals((object)num2);
+        }
+
+        public static bool operator !=( RationalNumber num1, RationalNumber num2)
+        {
+            return !num1.Equals((object)num2);
+        }
+
+        //--------Методы
+        public override bool Equals(object obj)
+        {
+            if(obj==null)
+            {
+                return false;
+            }
+            RationalNumber objtest = obj as RationalNumber;
+            if (objtest == null)
+            {
+                return false;
+            }
+            else
+                return Equals(objtest);
+        }
+
+        public bool Equals (RationalNumber  other)
+        {
+            if (other == null)
+                return false;
+            if (this._numerator == other._numerator && this._denominator == other._denominator)
+                return true;
+            else return false;
+        }
+
+        public static void Initialization(string input, out RationalNumber number)
+        {
+            int num, denom;
+            string[] cell = input.Split(' ','/');
+            if (cell.Length == 1&&Int32.TryParse(cell[0],out num))
+            {
+                number = new RationalNumber(num, 1);
+                return;
+            }                
+            else if (cell.Length == 2 && Int32.TryParse(cell[0], out num) && Int32.TryParse(cell[1], out denom) && denom > 0)
+            {
+                number = new RationalNumber(num, denom);
+                return;
+            }
+            else
+            {
+                throw new Exception("Input Error");
+            }              
+        }
+
+        public static  string ToStringIrreducible(RationalNumber result)
+        {
+            int gcd = GCD(result);
+            result._numerator /= gcd;
+            result._denominator /=gcd;
+            int full;
+            if (Math.Abs(result._numerator) > Math.Abs(result._denominator))
+            {
+                full = result._numerator / result._denominator;
+                return $"{full} {Math.Abs(result._numerator % result._denominator)}/{result._denominator}";
+            }
+            else
+                return $"{result._numerator}/{result._denominator}";
+        }
+
+        public static string ToString(RationalNumber result)
+        {
+            return $"{result._numerator}" +
+                "\n---" +
+                $"\n{result._denominator}";
+        }
+
+        private static int GCD(RationalNumber num)
+        {
+            int a = num._numerator;
+            int b = num._denominator;
+            if (a == 0)
+            {
+                return b;
+            }
+            while (b != 0)
+            {
+                if (Math.Abs(a) > Math.Abs(b))
+                   a -=Math.Abs( b);                
+                else                
+                    b -= Math.Abs(a);               
+            }
+            return Math.Abs(a);
+        }
+
+        public int CompareTo (object obj)
+        {
+            if (obj == null)
+                return 1;
+            RationalNumber objTransform = obj as RationalNumber;
+            if(objTransform!=null)
+            {
+                double num1 = _numerator / _denominator;
+                double num2 = objTransform._numerator / objTransform._denominator;
+                return num2.CompareTo(num1);
+            }
+            else
+            {
+                throw new ArgumentException("Object is a not rational number");
+            }
+            
+        }
+
+        //-------Перегрузка операций преобразования типов
+        public static implicit operator bool (RationalNumber num)
+        {
+            if (num._numerator!=0) return true;
+            else return false;
+        }
+
+        public static explicit operator int(RationalNumber num)
+        {
+            return num._numerator / num._denominator;
+        }
+
+        public static implicit operator double(RationalNumber num)
+        {
+            return (double)num._numerator /(double) num._denominator;
+        }   
+    }
+}

c78fcbebf104f1937968ba1b6158d87aff1a9052
953506 <62437619+953506@users.noreply.github.com>
2020-05-29 22:54:32 +0300

    Lab5

---
+++ chijik.danila/Лаба5/Car.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+    class Car : Transport
+    {
+        protected uint price = 0;
+        protected uint numberOfSeats;
+        protected uint trunkSize;
+
+        public enum CarType
+        {
+            Sedan,
+            Universal,
+            Hatchback,
+            Pickup,
+            Crossover,
+            SUV
+        }
+
+        public uint Price
+        {
+            get => price;
+            set => price = value;
+        }
+
+        public Car()
+        {
+            price = 0;
+            numberOfSeats = 0;
+            trunkSize = 0;
+        }
+
+        public virtual void TestDrive() { Console.WriteLine("Test drive went well!\a"); }
+
+        public void Calculate()
+        {
+            price = numberOfSeats * 20 + trunkSize * 2;
+            if (ComfortLevel == "high")
+                price *= 4;
+            else if (ComfortLevel == "medium")
+                price *= 2;
+        }
+        ~Car() { Console.WriteLine(""); }
+    }
---
+++ chijik.danila/Лаба5/Transport.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+    abstract class Transport
+    {
+        protected static int seriesNumber;
+        protected static int[] registrationNumber;
+        protected string name;
+        protected string color;
+        protected string comfortLevel;
+        protected uint yearMade;
+
+        public string Name
+        {
+            get => name;
+            set => name = value;
+        }
+
+        public string Color
+        {
+            get => color;
+            set => color = value;
+        }
+
+        public string ComfortLevel
+        {
+            get => comfortLevel;
+            set => comfortLevel = value;
+        }
+
+        public uint YearMade
+        {
+            get => yearMade;
+            set
+            {
+                if (value > 2020 || value < 1960)
+                    return;
+                else
+                    yearMade = value;
+            }
+        }
+
+        public static int Series
+        {
+            get => seriesNumber;
+        }
+
+        public int this[int digit]
+        {
+            get => registrationNumber[digit];
+        }
+
+        public Transport()
+        {
+            registrationNumber = new int[4];
+            Random rnd = new Random();
+            foreach (int digit in registrationNumber)
+            {
+                registrationNumber[digit] = rnd.Next(0, 9);
+            }
+            seriesNumber = rnd.Next(1000, 10000);
+        }
+        ~Transport() { }
+    }

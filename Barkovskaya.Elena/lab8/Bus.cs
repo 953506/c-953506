5b8005f37d60243d56c527530821242b94760f1f
953506 <62437619+953506@users.noreply.github.com>
2020-05-30 08:12:27 +0300

    Update Bus.cs

--- Barkovskaya.Elena/lab8/Bus.cs
+++ Barkovskaya.Elena/lab8/Bus.cs
@@ -6,15 +6,13 @@ namespace labr8
 {
     class Bus : MarkCar, ICarInfo
     {
-
-        string[] jobs = new string[] { "POHORONNYI", "Organisation Bus", "School bus", "City bus"};
+        string[] jobs = new string[] { "Funeral", "Organisation Bus", "School bus", "City bus"};
         public delegate void JobInformer(string message);
         public event JobInformer Notify;
         public string job;
         public int MaxSpeed { get; set; }
         public int MinSpeed { get; set; }
         public int FuelConsumption { get; set; }
-
 
         public Bus(string mark, string model, string color, string chassis_type, int country_choice, int max, int min, int fuel) : base(mark, model, color, chassis_type, country_choice)
         {
@@ -61,7 +59,7 @@ namespace labr8
         }
         public void GetRandomJob() 
         {
-            Notify?.Invoke("Now you get eandom JOB. Say \"JESUS Christ\" to up you chance to be Jesus Helper (POHORONNYI type)");
+            Notify?.Invoke("Now you get eandom JOB. Say \"JESUS Christ\" to up you chance to be Jesus Helper (Funeral type)");
             Random random = new Random();
             TakeJob((uint)random.Next(1, 4));
         }

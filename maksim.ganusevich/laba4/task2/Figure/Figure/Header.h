3cfa4b78c8f4792c2a7000ae9b804b0eec56ddb6
953506 <62437619+953506@users.noreply.github.com>
2020-05-30 08:30:49 +0300

    laba

--- maksim.ganusevich/laba4/task2/Figure/Figure/Header.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "Source.cpp"
-
-extern "C" __declspec(dllexport) double circle(float side, unsigned short parameterNumber);
-extern "C" __declspec(dllexport) double square(float side, unsigned short parameterNumber);
-extern "C" __declspec(dllexport) double triangle(float side, unsigned short parameterNumber);
-extern "C" __declspec(dllexport) double hexagon(float side, unsigned short parameterNumber);

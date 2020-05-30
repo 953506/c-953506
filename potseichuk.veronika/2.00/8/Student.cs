758db2fe879ad79286754f7438a578b8663913af
VeronikaPotseichuk <62996245+VeronikaPotseichuk@users.noreply.github.com>
2020-05-30 08:15:57 +0300

    Update Student.cs

--- potseichuk.veronika/2.00/8/Student.cs
+++ potseichuk.veronika/2.00/8/Student.cs
@@ -42,10 +42,10 @@ namespace Lab8
         {
             this.Stage = stage;
 
-            examsNotes[0].Subject = "Иностранный язык";
-            examsNotes[1].Subject = "ВышМат";
-            examsNotes[2].Subject = "Физкультура";
-            examsNotes[3].Subject = "Математическая логика";
+            examsNotes[0].Subject = "Foreign language";
+            examsNotes[1].Subject = "Higher math";
+            examsNotes[2].Subject = "Physical culture";
+            examsNotes[3].Subject = "Math logic";
 
             SetNotes();
         }
@@ -53,9 +53,9 @@ namespace Lab8
         public override void Display()
         {
             base.Display();
-            Console.WriteLine("Cтудент {0} курса", Stage);
+            Console.WriteLine("Student {0} stage",  Stage);
             ShowNotes();
-            Console.WriteLine("Рейтинг (в %): {0}", Rating);
+            Console.WriteLine("Rating (in %): {0}", Rating);
         }
         public void SetRating()
         {

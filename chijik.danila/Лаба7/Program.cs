d1d5d58c6b8c97c530d1483bb15cd962dd826ac0
953506 <62437619+953506@users.noreply.github.com>
2020-06-03 14:44:06 +0300

    Update Program.cs

--- chijik.danila/Лаба7/Program.cs
+++ chijik.danila/Лаба7/Program.cs
@@ -1,11 +1,10 @@
 ﻿using System;
-
+using static System.Console;
     class Program
     {
         static void Menu()
         {
-            Console.WriteLine();
-            Console.WriteLine("Выберите действие:\n" +
+            WriteLine("Выберите действие:\n" +
                               "1)Сложить два числа\n" +
                               "2)Вычесть из первого числа второе\n" +
                               "3)Умножить числа\n" +
@@ -19,12 +18,12 @@
         {
             RationalNumber a, b;
             int numerator, denominator;
-            Console.WriteLine("Введите числитель первой дроби");
+            WriteLine("Введите числитель первой дроби");
             numerator = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Введите знаменатель первой дроби");
+            WriteLine("Введите знаменатель первой дроби");
             denominator = Convert.ToInt32(Console.ReadLine());
             a = new RationalNumber(numerator, denominator);
-            Console.WriteLine("Введите число в формате a/b");
+            WriteLine("Введите число в формате a/b");
             string form = Console.ReadLine();
             b = new RationalNumber(form);
             int ch;
@@ -33,40 +32,40 @@
             {
                 Menu();
                 ch = Convert.ToInt32(Console.ReadLine());
-                Console.Clear();
+                Clear();
                 switch (ch)
                 {
                     case 1:
-                        Console.WriteLine($"{a + b} или {(double)(a + b)}");
+                        WriteLine($"{a + b} или {(double)(a + b)}");
                         break;
                     case 2:
-                        Console.WriteLine($"{a - b} или {(double)(a - b)}");
+                        WriteLine($"{a - b} или {(double)(a - b)}");
                         break;
                     case 3:
-                        Console.WriteLine($"{a * b} или {(double)(a * b)}");
+                        WriteLine($"{a * b} или {(double)(a * b)}");
                         break;
                     case 4:
-                        Console.WriteLine($"{a / b} или {(double)(a / b)}");
+                        WriteLine($"{a / b} или {(double)(a / b)}");
                         break;
                     case 5:
-                        if (a > b) Console.WriteLine($"{a} или {(double)(a)} больше");
-                        else if (a < b) Console.WriteLine($"{b} или {(double)(b)} больше");
-                        else Console.WriteLine("Числа не равны");
+                        if (a > b) {WriteLine($"{a} или {(double)(a)} больше");}
+                        else if (a < b) {WriteLine($"{b} или {(double)(b)} больше");}
+                        else {WriteLine("Числа не равны");}
                         break;
                     case 6:
-                        if (a < b) Console.WriteLine($"{a} или {(double)(a)} меньше");
-                        else if (a > b) Console.WriteLine($"{b} или {(double)(b)} меньше");
-                        else Console.WriteLine("Числа не равны");
+                        if (a < b) {WriteLine($"{a} или {(double)(a)} меньше");}
+                        else if {(a > b) WriteLine($"{b} или {(double)(b)} меньше");}
+                        else {WriteLine("Числа не равны");}
                         break;
                     case 7:
-                        if (a == b) Console.WriteLine("Числа равны");
-                        else Console.WriteLine("Числа не равны");
+                        if (a == b) {WriteLine("Числа равны");}
+                        else {WriteLine("Числа не равны");}
                         break;
                     case 8:
                         exit = true;
                         break;
                     default:
-                        Console.WriteLine("Что-то не так.");
+                        {WriteLine("Что-то не так.");}
                         break;
                 }
             } while (!exit);
